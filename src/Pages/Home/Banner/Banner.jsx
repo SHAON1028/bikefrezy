3937aa28a86c2dc5e899aef9281cddcdcfdfb355
SHAON1028 <shaonkhairul@gmail.com>
2022-11-26 02:19:22 +0600

    error

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -6,16 +6,16 @@ const Banner = () => {
         <div>
 
 
-            <div class="w-full bg-cover bg-center" style={{
+            <div className="w-full bg-cover bg-center" style={{
                 "height": "32rem",
                 backgroundImage: `url(${bike5})`,
             }}  >
-                <div class="flex items-center pl-10 lg:pl-80 h-full w-full bg-gray-900 bg-opacity-60">
-                    <div class="">
-                        <h1 class="text-gray-200 text-4xl  lg:text-6xl">Selling your bike?  <br/>
+                <div className="flex items-center pl-10 lg:pl-80 h-full w-full bg-gray-900 bg-opacity-60">
+                    <div className="">
+                        <h1 className="text-gray-200 text-4xl  lg:text-6xl">Selling your bike?  <br />
                             It's as simple as 123 </h1>
                         <p className='text-gray-200 w-4/6 lg:text-lg mt-4'>We buy & sell premium Road, Triathlon, Mountain and eBikes that  have a resale at reasonable price .</p>
-                        <button class="mt-4 px-4 py-2 bg-yellow-400 text-black text-sm uppercase font-medium rounded hover:bg-blue-500 focus:outline-none focus:bg-blue-500">Explore Now</button>
+                        <button className="mt-4 px-4 py-2 bg-yellow-400 text-black text-sm uppercase font-medium rounded hover:bg-blue-500 focus:outline-none focus:bg-blue-500">Explore Now</button>
                     </div>
                 </div>
             </div>
