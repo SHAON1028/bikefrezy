89f7c8aa23da0a61c5b9f5e852e1ee80841c0cfb
SHAON1028 <shaonkhairul@gmail.com>
2022-12-22 17:53:46 +0600

    authentication ok

--- src/Layout/DashboardLayout/DashboardLayout.jsx
+++ src/Layout/DashboardLayout/DashboardLayout.jsx
@@ -4,6 +4,7 @@ import { Link, NavLink, Outlet } from 'react-router-dom';
 import { AuthContext } from '../../contexts/AuthProvider';
 import useAdmin from '../../Hooks/useAdmin';
 import useSeller from '../../Hooks/useSeller';
+import GetLoader from '../../Pages/shared/GetLoader/GetLoader';
 
 import Navbar from '../../Pages/shared/Navbar/Navbar';
 
@@ -50,8 +51,10 @@ const DashboardLayout = () => {
                 <div className="drawer-side">
                      <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
                     <ul className="menu p-4 lg:w-80 w-40 bg-base-100 text-base-content"> 
+                 
 
                         {
+                           
                             !isAdmin && !isSeller && <Link to="/dashboard/myorder" className="flex items-center p-2 text-base font-normal bg-gray-700 rounded-lg dark:text-white hover:bg-green-500 hover:text-black mb-2">
                                 <svg aria-hidden="true" className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd"></path></svg>
                                 <span className="flex-1 ml-3 whitespace-nowrap">My orders</span>
